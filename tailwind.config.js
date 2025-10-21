e353cb28375cdea22b763a762e23716a2054b410
ampan19 <ampan.0919@gmail.com>
2025-10-21 16:47:42 +0700

    update dark mode + perbaikan fitur

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['index.html'],
+  darkMode: 'class',
   theme: {
     container: {
       center: true,
@@ -8,9 +9,17 @@ module.exports = {
     },
     extend: {
         colors: {
-          primary: '#14b8a6',
-          secondry: '#64748b',
-          dark: '#09090b'
+          //kalo halaman light
+          primary: '#000', //hitam 
+          secondry: 'oklch(70.4% 0.14 182.503)', //biru teal aksen------------
+          deskripsi: 'oklch(26.8% 0.007 34.298)', //hitam tdk terlalu stone800
+          
+          //kalo halaman dark
+          dark: 'oklch(26.8% 0.007 34.298)', //hitam------------- stone 800
+          deskripsi1: 'oklch(86.9% 0.005 56.366)', //putih pudar 
+          primary1: 'oklch(96.7% 0.001 286.375)', //putih
+          instagram: '#FF0069',
+          facebook:'#0866FF'
         },
         screens: {
           '2xl' : '1320px',
